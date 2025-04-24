68cb3eca87eea1154cca0d6bb9dbb5ec05f82de8
z7md <z7mdd90@gmail.com>
2025-04-24 04:57:06 +0300

    form edit

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -3,6 +3,9 @@ import api from "../api"
 import { useNavigate } from "react-router-dom"
 import { ACCESS_TOKEN, REFRESH_TOKEN } from "../constants"
 import "../styles/form.css"
+import {Footer} from "./Footer"
+import "../app.css"
+import { FaWhatsapp } from "react-icons/fa";
 
 function Form({ route, method }) {
     const [username, setUsername] = useState("");
@@ -10,13 +13,14 @@ function Form({ route, method }) {
     const [loading, setLoading] = useState(false)
     const navigate = useNavigate()
 
-    const name = method === "login" ? "Login" : "Register"
+    const name = method === "login" ? "تسجيل دخول" : "Register"
     const handleSubmit = async(e) => {
         setLoading(true)
         e.preventDefault()
 
         try{
             const res = await api.post(route,{username,password})
+            console.log({username,password})
             if (method==="login"){
                 localStorage.setItem(ACCESS_TOKEN,res.data.access)
                 localStorage.setItem(REFRESH_TOKEN,res.data.refresh)
@@ -31,15 +35,16 @@ function Form({ route, method }) {
         }
 
     }
-    return <form onSubmit={handleSubmit} className="form-container">
-        <h1>
-            {name}1
+    return <div className="rubik"><form onSubmit={handleSubmit} className="form-container">
+        <h1 className="text-4xl mb-2">
+            {name} 
+
         </h1>
         <input className="form-input"
             type="text"
             value={username}
             onChange={(e) => setUsername(e.target.value)}
-            placeholder="Username"
+            placeholder="أسم المستخدم"
             autoComplete="on"
         />
 
@@ -47,13 +52,22 @@ function Form({ route, method }) {
             type="password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
-            placeholder="Password"
+            placeholder="كلمة المرور"
             autoComplete="on"
         />
         <button className="form-button" type="submit">
             {name}
         </button>
     </form>
+
+    <p className="text-center font-bold text-2xl">لشراء اللعبة يرجى التواصل عبر الواتساب بالأسفل </p>
+    <div className="text-center m-4 text-2xl">سعر اللعبة <span className="text-green-500 font-bold font-stretch-105%">11.99 ريال</span> </div>
+    <div className="max-w-5xl mx-auto px-4 flex justify-center items-center gap-8">
+            
+             <a href="https://wa.me/966508559192" className="opacity-70 hover:opacity-100 cursor-pointer text-white"><FaWhatsapp className="size-12" /></a>
+
+         </div>
+    </div>
 }
 
 export default Form
